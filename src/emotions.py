f84bab0b31e41cbb9a6397a65dff6e612011cf16
mitalipatil99 <patilmitali99@gmail.com>
2024-01-11 20:20:56 +0100

    main file added with facial and emotion recog

--- src/emotions.py
+++ src/emotions.py
@@ -94,68 +94,70 @@ model.add(Dense(1024, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(7, activation='softmax'))
 
-# If you want to train the same model or try other models, go for this
-if mode == "train":
-    model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
-    # model_info = model.fit_generator(
-    #     train_generator,
-    #     steps_per_epoch=num_train // batch_size,
-    #     epochs=num_epoch,
-    #     validation_data=validation_generator,
-    #     validation_steps=num_val // batch_size)
-    # plot_model_history(model_info)
-    model.save_weights('model.h5')
 
-# emotions will be displayed on your face from the webcam feed
-elif mode == "display":
-    model.load_weights('model.h5')
+def emotion_detection(mode):
+    # If you want to train the same model or try other models, go for this
+    if mode == "train":
+        model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
+        # model_info = model.fit_generator(
+        #     train_generator,
+        #     steps_per_epoch=num_train // batch_size,
+        #     epochs=num_epoch,
+        #     validation_data=validation_generator,
+        #     validation_steps=num_val // batch_size)
+        # plot_model_history(model_info)
+        model.save_weights('model.h5')
 
-    # prevents openCL usage and unnecessary logging messages
-    cv2.ocl.setUseOpenCL(False)
+    # emotions will be displayed on your face from the webcam feed
+    elif mode == "display":
+        model.load_weights('model.h5')
 
-    # dictionary which assigns each label an emotion (alphabetical order)
-    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+        # prevents openCL usage and unnecessary logging messages
+        cv2.ocl.setUseOpenCL(False)
 
-    # start the webcam feed
-    cap = cv2.VideoCapture(0)
+        # dictionary which assigns each label an emotion (alphabetical order)
+        emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
-    emotion_ten_array = []
+        # start the webcam feed
+        cap = cv2.VideoCapture(0)
 
-    while True:
-        # Find haar cascade to draw bounding box around face
-        ret, frame = cap.read()
-        if not ret:
-            break
-        facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+        emotion_ten_array = []
 
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-            prediction = model.predict(cropped_img)
-            maxindex = int(np.argmax(prediction))
-            cv2.putText(frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),
-                        2, cv2.LINE_AA)
+        while True:
+            # Find haar cascade to draw bounding box around face
+            ret, frame = cap.read()
+            if not ret:
+                break
+            facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
-            if len(emotion_ten_array) == 10:
-                print(emotion_ten_array)
-                user_emotion = md(emotion_ten_array)
-                print(user_emotion)
-                emotion_ten_array.clear()
-            else:
-                emotion_ten_array.append(emotion_dict[maxindex])
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
+                roi_gray = gray[y:y + h, x:x + w]
+                cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+                prediction = model.predict(cropped_img)
+                maxindex = int(np.argmax(prediction))
+                cv2.putText(frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),
+                            2, cv2.LINE_AA)
 
-        cv2.imshow('Video', cv2.resize(frame, (1600, 960), interpolation=cv2.INTER_CUBIC))
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        time.sleep(1)
+                if len(emotion_ten_array) == 10:
+                    print(emotion_ten_array)
+                    user_emotion = md(emotion_ten_array)
+                    print(user_emotion)
+                    emotion_ten_array.clear()
+                else:
+                    emotion_ten_array.append(emotion_dict[maxindex])
 
-    cap.release()
-    cv2.destroyAllWindows()
+            cv2.imshow('Video', cv2.resize(frame, (1600, 960), interpolation=cv2.INTER_CUBIC))
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            time.sleep(1)
 
-    # every second, 10 emotions, return mode emotion,
-    # update emotion col in  database only after id and name is updated.
-    # update database every 10 seconds.
-    # sqllite
+        cap.release()
+        cv2.destroyAllWindows()
+
+        # every second, 10 emotions, return mode emotion,
+        # update emotion col in  database only after id and name is updated.
+        # update database every 10 seconds.
+        # sqllite
